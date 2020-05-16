b687a0f40dad756794229929e2fa413ca2026acc
Adrian Bercovici <adriansan_93@yahoo.com>
2020-05-16 21:28:29 +0300

    end of day commit

--- wk.erl
+++ wk.erl
@@ -20,12 +20,12 @@ init(Limit)->
 
 
 handle_call(From,state,State)->
-    {reply,33,State};
+    {reply,State,State};
 handle_call(From,Message,State=#state{processed=P,limit=L,counter=C})->
      Reply=if C=:=L;C>L -> exit(consumed);
               C<L       -> {{processed,self(),os:timestamp()},Message}
      end,
-    {reply,Reply,State#state{limit=L+1,processed=[Message,P]}}.
+    {reply,Reply,State#state{counter=C+1,processed=[Message,P]}}.
 
 handle_info(Message,State=#state{unknown=U})->
     {noreply,State#state{unknown=[Message|U]}}.
