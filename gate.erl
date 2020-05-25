4de31fefd9572bc4805f15dc443597dab5220434
Adrian Bercovici <adriansan_93@yahoo.com>
2020-05-25 08:21:39 +0300

    works

--- gate.erl
+++ gate.erl
@@ -3,23 +3,33 @@
 -compile(export_all).
 -record(gst,#{
     sup,
-    refs
+    refs,
+    n,
+    queue
 }).
 
 process(Message,Server)->
     gen_server:call(Server,Message).
 
 
-start(Sup)->
-    gen_server:start({local,?MODULE},?MODULE,[Sup]).
-start_link(Sup)->
-   gen_server:start_link({local,?MODULE},?MODULE,[Sup]).
+start(Sup,Args)->
+    gen_server:start({local,?MODULE},?MODULE,[Sup],[]).
+start_link(Sup,Args)->
+   gen_server:start_link({local,?MODULE},?MODULE,Args,[]).
 
-init(Sup)->
-    {ok,#state{sup=Sup,refs=gb_sets:new()}}.
+init({Sup,N})->
+    {ok,#state{sup=Sup,refs=gb_sets:new(),n=N,queue=queue:new()}}.
 
-handle_call(From,Message,State=#state{sup=Sup,refs=Refs})->
-     {ok,X}=supervisor:start_child(Sup,[3]),
+handle_cast(From,Message,State=#state{sup=Sup,refs=Refs,n=N}) when L>0 ->
+     {ok,Pid}=supervisor:start_child(Sup,[3]),
+     Ref=erlang:monitor(Pid),
+     {noreply,State#state{refs=gb_sets:insert(Ref,Refs)}}.
+
+handle_cast(From,Message,State=#state{queue=Q}) when L<=0 ->
+    {noreply,State#state{quque=queue:in(Message,)}}
+  
+handle_info({'DOWN',Ref,process,Pid,_},State)->
+
 %api
 
 
