de68465053a30b7b00eb85a63e800254c37f19ca
Adrian Bercovici <adriansan_93@yahoo.com>
2020-06-06 10:34:18 +0300

    adding monitors

--- mq.erl
+++ mq.erl
@@ -4,42 +4,49 @@
     queue,
     queueCount
 }).
--record(procstate,{
-    
+-record(sstate,{
+    eref,
+    pref
 }).
 -define(QUEUE_SIZE,5).
 -define(PROC_SLEEP,1000).
+-compile(export_all).
 
 start()->
-    spawn(?MODULE,init,[]).
-init()->
-    EnqueuerPid=spawn(?MODULE,enqueuer,#enqstate{queue=queue:new(),queueCount=0}),
-    ProcessorPid=spawn(?MODULE,processor,#procstate{}),
-    loop(EnqueuerPid).
+    spawn(?MODULE,server,[]).
+server()->
+    EnqueuerPid=spawn(?MODULE,enqueuer,[#enqstate{queue=queue:new(),queueCount=0}]),
+    ProcessorPid=spawn(?MODULE,processor,[]),
+    ERef=erlang:monitor(EnqueuerPid),
+    PRef=erlang:monitor(ProcessorPid),
+    loop(#sstate{eref=ERef,pref=PRef}).
 
-loop(EnqueuerPid) ->
+loop(S=#sstate{eref=E,pref=P}) ->
     receive
+        {'DOWN',E,process,Pid,Reason}->exit(enq_down);
+        {'DOWN',P,process,Pid,Reason}->exit(proc_down);
         {From,Msg} -> EnqueuerPid ! {From,Msg};
          _ -> exit(invalid_message) 
-    end
+    end.
 
 
-enqueuer(State#enqstate{queueCount=C,queue=Q})
+enqueuer(State=#enqstate{queueCount=C,queue=Q})->
     receive 
-        {From,MSG}-> if C<?QUEUE_SIZE -> NewQueue=queue:in({FROM,MSG},Q),
+        {From,MSG}-> if C<?QUEUE_SIZE -> NewQueue=queue:in({From,MSG},Q),
                                          enqueuer(State#enqstate{queueCount=C+1,queue=NewQueue});
                          true         -> From ! {queue_full_try_later},
                                          enqueuer(State)
-                     end
+                     end;
         {ProcessorPid,msg_processed}-> 
                                  case queue:out(Q) of
                                      {{value,Element},RemQueue} ->
                                               ProcessorPid ! {self(),Element},
-                                              enqueuer(State#state{queueCount=C-1,queue=RemQueue});
+                                              enqueuer(State#enqstate{queueCount=C-1,queue=RemQueue});
                                       {empty,_} -> enqueuer(State)
+                                 end
     end.
         
-processor(ProcState#procstate)->
+processor()->
     receive 
         {EnqPid,{From,Message}}->
                 timer:sleep(?PROC_SLEEP),
@@ -47,5 +54,5 @@ processor(ProcState#procstate)->
                 EnqPid ! {self(),msg_processed},
                 processor(ProcState);
 
-         _ ->processor(ProcState)
+         _ -> exit(wrong_message)
     end.
